54a4ddadcb714f4ed8f29be1cb4fc82a89d098ff
Kunal Agarwal <kunal.r.agarwal@gmail.com>
2025-11-07 15:35:44 +0530

    add(profile-service): add get profile by id api implementation

---
+++ profile-service/src/main/java/com/portfolio/elite/profile/service/impl/ProfileServiceMock.java
@@ -0,0 +1,28 @@
+package com.portfolio.elite.profile.service.impl;
+
+import com.portfolio.elite.profile.service.IProfileService;
+import com.portfolio.elite.profileservice.v1.model.GetProfileResponse;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("mock")
+@Slf4j
+public class ProfileServiceMock implements IProfileService {
+
+    @Override
+    public GetProfileResponse findProfileById(String id) {
+        return new GetProfileResponse()
+            .id(id)
+            .age(32)
+            .dob("1994-06-10")
+            .gender("MALE");
+    }
+
+    @PostConstruct
+    protected void log() {
+        log.debug("ProfileService is activated in mock mode");
+    }
+}
