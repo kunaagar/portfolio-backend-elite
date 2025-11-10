f5ec16c0315dc062557620f95920fc08f76f9dc6
Kunal Agarwal <kunal.r.agarwal@gmail.com>
2025-11-10 10:34:54 +0530

    add(customer-service): add plugin to generate profile-service feign client

--- customer-service/src/main/java/com/portfolio/elite/customerservice/service/impl/CustomerServiceMock.java
+++ customer-service/src/main/java/com/portfolio/elite/customerservice/service/impl/CustomerServiceMock.java
@@ -1,9 +1,11 @@
 package com.portfolio.elite.customerservice.service.impl;
 
 
+import com.portfolio.elite.clients.profileservice.v1.api.ProfilesApiClient;
 import com.portfolio.elite.customerservice.service.ICustomerService;
 import com.portfolio.elite.customerservice.v1.model.Customer;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,15 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @Profile("mock")
+@RequiredArgsConstructor
 public class CustomerServiceMock implements ICustomerService {
+
+    private final ProfilesApiClient client;
 
     @Override
     public Customer findCustomerById(String id) {
+        final var result = client.getProfileById("cust-001");
+        log.debug("Profile service response: {}", result);
         return new Customer().id("cust-001").name("customer-001").email("cust@mock.com");
     }
 
