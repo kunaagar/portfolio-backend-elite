84628a0b65e2b30d22cdabf2a790faa068f77812
Kunal Agarwal <kunal.r.agarwal@gmail.com>
2025-11-10 10:34:54 +0530

    add(customer-service): add plugin to generate profile-service feign client

--- customer-service/src/main/java/com/portfolio/elite/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/portfolio/elite/customerservice/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.portfolio.elite.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.portfolio.elite.clients.*")
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
