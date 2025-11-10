5ee0e8b1919e5afa2a5ac2a84661bbd8511d2b26
Kunal Agarwal <kunal.r.agarwal@gmail.com>
2025-11-10 17:43:33 +0530

    chore(customer-service): update customer-service integration to consume profile-service api using feign client

--- customer-service/src/main/java/com/portfolio/elite/customerservice/service/impl/CustomerServiceMock.java
+++ customer-service/src/main/java/com/portfolio/elite/customerservice/service/impl/CustomerServiceMock.java
@@ -1,9 +1,11 @@
 package com.portfolio.elite.customerservice.service.impl;
 
 
-import com.portfolio.elite.clients.profileservice.v1.api.ProfilesApiClient;
+import com.portfolio.elite.clients.profileservice.v1.model.GetProfileResponse;
+import com.portfolio.elite.customerservice.provider.ProfileServiceProvider;
 import com.portfolio.elite.customerservice.service.ICustomerService;
 import com.portfolio.elite.customerservice.v1.model.Customer;
+import com.portfolio.elite.customerservice.v1.model.CustomerProfileInfo;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,13 +18,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerServiceMock implements ICustomerService {
 
-    private final ProfilesApiClient client;
+    private final ProfileServiceProvider profileServiceProvider;
 
     @Override
     public Customer findCustomerById(String id) {
-        final var result = client.getProfileById("cust-001");
-        log.debug("Profile service response: {}", result);
-        return new Customer().id("cust-001").name("customer-001").email("cust@mock.com");
+        //TODO :: handle case when profile response is not available
+        final var profileResponse = profileServiceProvider.getProfileById(id).orElseGet(GetProfileResponse::new);
+        return new Customer().id("cust-001").name("customer-001").email("cust@mock.com")
+            .profileInfo(new CustomerProfileInfo().gender(profileResponse.getGender())
+                .age(profileResponse.getAge()).dob(profileResponse.getDob()));
     }
 
     @PostConstruct
