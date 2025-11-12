bbc877afe344e3813a230bd8dc86bb2b5992023d
Kunal Agarwal <kunal.r.agarwal@gmail.com>
2025-11-12 11:18:38 +0530

    feat(customer-service): added observability on profile-service api call to generate custom metrics

--- customer-service/src/main/java/com/portfolio/elite/customerservice/provider/impl/ProfileServiceProviderImpl.java
+++ customer-service/src/main/java/com/portfolio/elite/customerservice/provider/impl/ProfileServiceProviderImpl.java
@@ -3,19 +3,22 @@ package com.portfolio.elite.customerservice.provider.impl;
 import com.portfolio.elite.clients.profileservice.v1.api.ProfilesApiClient;
 import com.portfolio.elite.clients.profileservice.v1.model.GetProfileResponse;
 import com.portfolio.elite.customerservice.provider.ProfileServiceProvider;
+import io.micrometer.core.annotation.Timed;
 import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class ProfileServiceProviderImpl implements ProfileServiceProvider {
 
     private final ProfilesApiClient client;
 
+    @Timed(value = "feign.call.profile.api", description = "Time taken to call profile service api")
     @Override
     public Optional<GetProfileResponse> getProfileById(String id) {
         final var response = client.getProfileById(id);
