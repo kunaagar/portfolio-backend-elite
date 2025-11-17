7266ec14f7ee3dff960dc44a9ff6e5c7f82a70ec
Kunal Agarwal <kunal.r.agarwal@gmail.com>
2025-11-17 15:14:48 +0530

    feat(logback) - add logback-spring.xml to include traceid, spanId etc

--- customer-service/src/main/java/com/portfolio/elite/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/portfolio/elite/customerservice/CustomerServiceApplication.java
@@ -1,15 +1,27 @@
 package com.portfolio.elite.customerservice;
 
+import jakarta.servlet.Filter;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.portfolio.elite.clients.*")
+@Slf4j
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner runner(ApplicationContext ctx) {
+		return args -> Arrays.stream(ctx.getBeanNamesForType(Filter.class))
+			.forEach(bean -> log.info("Filter bean present: {}", bean));
+	}
 }
